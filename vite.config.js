8e2696a4098c17769be5a2a34cb5c651955e1baf
5711 - Vysakh Venugopalan <5711vyv@manorama.net>
2024-08-22 15:27:24 +0530

    added repo name to vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React_background-colour-changer",
+  base: "/React_background-colour-changer/",
   plugins: [react()],
 })
 
